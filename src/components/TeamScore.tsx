2c1c0f58e540d6f9db60284dd767f8d305a1d0e7
Joshua Lehmann <joshua.le1999@gmail.com>
2025-02-15 15:52:52 +0100

    Increased text size

--- src/components/TeamScore.tsx
+++ src/components/TeamScore.tsx
@@ -11,13 +11,13 @@ export function TeamScore(props: TeamScoreProps) {
   return (
     <div class="flex w-full max-w-[14rem] flex-col items-center text-center">
       <h3
-        class="line-clamp-2 min-h-[2rem] overflow-hidden text-sm leading-tight font-bold md:text-xl"
+        class="line-clamp-2 min-h-[2rem] overflow-hidden text-lg leading-tight font-bold md:text-xl"
         style={{ display: "-webkit-box", "-webkit-box-orient": "vertical" }}
       >
         {props.teamName}
       </h3>
 
-      <div class="my-2 text-3xl font-bold">{props.score}</div>
+      <div class="my-2 text-9xl font-bold">{props.score}</div>
       <div class="mt-1 flex gap-2">
         <ScoreButton direction={1} updateScore={() => props.updateScore(1)} />
         <ScoreButton direction={-1} updateScore={() => props.updateScore(-1)} />
