cd03070df5fbf8f73812d73fc095492268cdadd6
Joshua Lehmann <joshua.le1999@gmail.com>
2025-03-13 13:43:06 +0100

    Add lucid icons

--- src/routes/MainLayout.tsx
+++ src/routes/MainLayout.tsx
@@ -6,40 +6,38 @@ import { Login } from "../components/auth/Login.tsx";
 
 export const MainLayout: ParentComponent = (props) => {
   return (
-    <>
-      <div class={"flex h-screen flex-col"}>
-        <div class="navbar bg-base-100 h-20 shadow-sm">
-          <div class="navbar-start">
-            <A class="btn btn-ghost text-xl" href={"/"}>
-              Foosball Tracker
-            </A>
-            <div class="flex gap-2">
-              <MqttStatus />
-              <ThemeSwitch />
-            </div>
-          </div>
-          <div class="navbar-center flex">
-            <ul class="menu menu-horizontal px-1">
-              <li>
-                <A href={"/players"} activeClass={"menu-active"}>
-                  Players
-                </A>
-              </li>
-
-              <li>
-                <A href={"/teams"} activeClass={"menu-active"}>
-                  Teams
-                </A>
-              </li>
-            </ul>
-          </div>
-          <div class="navbar-end">
-            <Login />
+    <div class={"flex h-screen flex-col"}>
+      <div class="navbar bg-base-100 h-20 shadow-sm">
+        <div class="navbar-start">
+          <A class="btn btn-ghost text-xl" href={"/"}>
+            Foosball Tracker
+          </A>
+          <div class="flex gap-2">
+            <MqttStatus />
+            <ThemeSwitch />
           </div>
         </div>
-        <div class={"flex-1 overflow-y-auto"}>{props.children}</div>
-        {/*Actual Component rendered based on the route*/}
+        <div class="navbar-center flex">
+          <ul class="menu menu-horizontal px-1">
+            <li>
+              <A href={"/players"} activeClass={"menu-active"}>
+                Players
+              </A>
+            </li>
+
+            <li>
+              <A href={"/teams"} activeClass={"menu-active"}>
+                Teams
+              </A>
+            </li>
+          </ul>
+        </div>
+        <div class="navbar-end">
+          <Login />
+        </div>
       </div>
-    </>
+      <div class={"flex-1 overflow-y-auto"}>{props.children}</div>
+      {/*Actual Component rendered based on the route*/}
+    </div>
   );
 };
