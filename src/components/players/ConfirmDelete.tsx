42257879da27702062161ad7e26433f76e639322
NathanielBirhanu <nbirhanu98@gmail.com>
2025-04-07 14:19:29 +0300

    used colors based on DaisyUI

--- src/components/players/ConfirmDelete.tsx
+++ src/components/players/ConfirmDelete.tsx
@@ -12,13 +12,13 @@ const ConfirmDelete = (props: ConfirmDeleteProps) => {
   return (
     <Show when={props.showConfirm}>
       <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-        <div class="bg-white p-6 rounded-lg shadow-lg w-80">
+        <div class="bg-base-300 p-6 rounded-lg shadow-lg w-80">
           <h2 class="text-lg font-semibold mb-4">Confirm Delete</h2>
           <p class="mb-4">
             Are you sure you want to delete <strong>{props.playerToDelete?.name}</strong>?
           </p>
           <div class="flex justify-end gap-2">
-            <button class="btn" onClick={props.onCancel}>
+            <button class="btn btn-outline" onClick={props.onCancel}>
               Cancel
             </button>
             <button class="btn btn-error" onClick={props.onConfirm}>
